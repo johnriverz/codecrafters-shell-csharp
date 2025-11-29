a46a00d7f5dae655658fc55494c44f9afe0bb8fa
johnriverz <juan.jose.rios.jr@gmail.com>
2025-11-29 12:01:27 -0800

    Handle invalid commands

--- src/main.cs
+++ src/main.cs
@@ -4,5 +4,10 @@ class Program
     {
         // TODO: Uncomment the code below to pass the first stage
         Console.Write("$ ");
+        while (true)
+        {
+            var command = Console.ReadLine();
+            Console.WriteLine($"{command}: command not found");
+        }
     }
 }
