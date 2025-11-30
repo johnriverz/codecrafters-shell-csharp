5b69ecc0e9fdfa3350b978d4792bfbd32955d647
johnriverz <juan.jose.rios.jr@gmail.com>
2025-11-29 17:40:42 -0800

    Implement a REPL

--- src/main.cs
+++ src/main.cs
@@ -3,9 +3,9 @@ class Program
     static void Main()
     {
         // TODO: Uncomment the code below to pass the first stage
-        Console.Write("$ ");
         while (true)
         {
+            Console.Write("$ ");
             var command = Console.ReadLine();
             Console.WriteLine($"{command}: command not found");
         }
